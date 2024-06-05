283af1538cafed9b5d4c3d7450eca338a4103b48
mango2309 <sebastian.largo@udla.edu.ec>
2024-06-05 12:18:30 -0500

    Modificacion del CodeBehind del Grid

--- SebastianLargo_PruebaProgresoII/Views/SL_ConteoCaracteres.xaml.cs
+++ SebastianLargo_PruebaProgresoII/Views/SL_ConteoCaracteres.xaml.cs
@@ -6,4 +6,33 @@ public partial class SL_ConteoCaracteres : ContentPage
 	{
 		InitializeComponent();
 	}
+	private void OnCalculateClicked(object sender, EventArgs e)
+	{
+		string input = InputEntry.Text ?? string.Empty;
+
+		int totalCharactersSL = input.Length;
+		int letterCountSL = input.Count(char.IsLetter);
+		int numberCountSL = input.Count(char.IsDigit);
+		int vowelCountSL = input.Count(c => "aeiouAEIOU".Contains(c));
+		int uppercaseCountSL = input.Count(char.IsUpper);
+		int lowercaseCountSL = input.Count(char.IsLower);
+
+		TotalCharactersLabel.Text = $"{totalCharactersSL}";
+		LetterCountLabel.Text = $"{letterCountSL}";
+		NumberCountLabel.Text = $"{numberCountSL}";
+		VowelCountLabel.Text = $"{vowelCountSL}";
+		UppercaseCountLabel.Text = $"{uppercaseCountSL}";
+		LowercaseCountLabel.Text = $"{lowercaseCountSL}";
+	}
+
+	private void OnClearClicked(object sender, EventArgs e)
+	{
+		InputEntry.Text = string.Empty;
+		TotalCharactersLabel.Text = string.Empty;
+		LetterCountLabel.Text = string.Empty;
+		NumberCountLabel.Text = string.Empty;
+		VowelCountLabel.Text = string.Empty;
+		UppercaseCountLabel.Text = string.Empty;
+		LowercaseCountLabel.Text = string.Empty;
+	}
 }
